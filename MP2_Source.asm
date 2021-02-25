5cdff63d60e16e3f3a01b3e143b798fda3a71b33
Jay Helm <45608814+SgtBacon@users.noreply.github.com>
2021-02-25 13:31:54 -0800

    Add project files.

---
+++ MP2_Source.asm
@@ -0,0 +1,28 @@
+TITLE MASM Template	(main.asm)
+; Description:    Number Value Program 
+; Authors:         
+; Date:           
+INCLUDE Irvine32.inc    
+; TODO: PLACE SYMBOLIC INTEGER CONSTANTS HERE 
+;
+.data
+; TODO: MAKE A LABEL numeros THAT LOCATES MEMORY FOR ALL THE DWORD NUMBER VALUES
+; USE THE INTEGER CONSTANTS SPECIFIED ABOVE TO INITALIZE ALL 10 DWORD VALUES
+; PUT THE NUMBER CONSTANTS IN MEMORY IN DESCENDING ORDER SO THAT THE OUTPUT FROM YOUR
+; PROGRAM MATCHES THAT SHOWN BELOW
+.code
+main proc
+       ; The code for main has already been written for you...
+       ; Setup a loop that will run 10 times and output all the DWORD number values
+	mov ecx, 10
+	mov esi, 0   
+	label1:
+	    ; This writes out each DWORD from the array "numeros" to the screen 
+	    mov eax,numeros[esi*TYPE DWORD]
+	    call WriteInt
+	    ; This increments the array index counter
+	    inc esi
+	LOOP label1  ; Loop 10 times (based on value in ECX)
+	invoke ExitProcess,0
+main endp
+end main
